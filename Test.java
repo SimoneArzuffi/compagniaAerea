cbd1b34309b4866c5ad7e74cb54c792223fd78a6
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2023-09-11 15:53:07 +0000

    finito

--- Test.java
+++ Test.java
@@ -1,9 +1,9 @@
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.Assert.*;
+
 public class Test {
 
-    private CompagniaAerea compagniaAerea;
+    public CompagniaAerea compagniaAerea;
 
     @Before
     public void setUp() {
@@ -40,20 +40,20 @@ public class Test {
     public void testModificaCliente() {
         compagniaAerea.aggiungiCliente("John", "Doe", "USA", "New York", "1990-01-15");
         compagniaAerea.modificaCliente("FL001", "Jane", "Doe", "USA", "Los Angeles", "1992-03-20");
-        assertEquals("Jane", compagniaAerea.clienti.get("FL001").getNome());
+        assertEquals("Jane", compagniaAerea.clienti.get("FL001").nome);
     }
 
     @Test
     public void testModificaVolo() {
         compagniaAerea.aggiungiVolo("FL001", "JFK", "LAX", "2023-09-15", "08:00", "13:00", 150, 300);
         compagniaAerea.modificaVolo("FL001", "JFK", "SFO", "2023-09-15", "08:00", "13:00", 150, 350);
-        assertEquals("SFO", compagniaAerea.voli.get("FL001").getAeroportoArrivo());
+        assertEquals("SFO", compagniaAerea.voli.get("FL001").aeroportoArrivo);
     }
 
     @Test
     public void testRicercaCliente() {
         Cliente cliente = new Cliente("John", "Doe", "USA", "New York", "1990-01-15", "FL001");
-        compagniaAerea.clienti.put(cliente.getCodiceCliente(), cliente);
+        compagniaAerea.clienti.put(cliente.codiceCliente, cliente);
         assertEquals(cliente, compagniaAerea.ricercaCliente("FL001"));
     }
 
@@ -61,17 +61,17 @@ public class Test {
     public void testRicercaClientePerNomeECognome() {
         Cliente cliente1 = new Cliente("John", "Doe", "USA", "New York", "1990-01-15", "FL001");
         Cliente cliente2 = new Cliente("Jane", "Doe", "USA", "Los Angeles", "1992-03-20", "FL002");
-        compagniaAerea.clienti.put(cliente1.getCodiceCliente(), cliente1);
-        compagniaAerea.clienti.put(cliente2.getCodiceCliente(), cliente2);
+        compagniaAerea.clienti.put(cliente1.codiceCliente, cliente1);
+        compagniaAerea.clienti.put(cliente2.codiceCliente, cliente2);
 
-        assertEquals(cliente1, compagniaAerea.ricercaClientePerNomeECognome("John", "Doe"));
-        assertEquals(cliente2, compagniaAerea.ricercaClientePerNomeECognome("Jane", "Doe"));
+        assertEquals(cliente1, compagniaAerea.ricercaClientePErNomeECognome("John", "Doe"));
+        assertEquals(cliente2, compagniaAerea.ricercaClientePErNomeECognome("Jane", "Doe"));
     }
 
     @Test
     public void testRicercaVolo() {
         Volo volo = new Volo("FL001", "JFK", "LAX", "2023-09-15", "08:00", "13:00", 150, 300);
-        compagniaAerea.voli.put(volo.getCodiceVolo(), volo);
+        compagniaAerea.voli.put(volo.codiceVolo, volo);
         assertEquals(volo, compagniaAerea.ricercaVolo("FL001"));
     }
 
@@ -79,8 +79,8 @@ public class Test {
     public void testNuovaPrenotazione() {
         Cliente cliente = new Cliente("John", "Doe", "USA", "New York", "1990-01-15", "FL001");
         Volo volo = new Volo("FL001", "JFK", "LAX", "2023-09-15", "08:00", "13:00", 150, 300);
-        compagniaAerea.clienti.put(cliente.getCodiceCliente(), cliente);
-        compagniaAerea.voli.put(volo.getCodiceVolo(), volo);
+        compagniaAerea.clienti.put(cliente.codiceCliente cliente);
+        compagniaAerea.voli.put(volo.codiceVolo, volo);
 
         compagniaAerea.nuovaPrenotazione("FL001", "FL001", 2, 20);
 
